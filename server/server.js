e1162d5bfabeec1f8383732c634bbfec02829092
Rohan-JD <rohanjdaniel@gmail.com>
2026-03-05 19:18:43 +0530

    Implement user login and bio update functionality
    Refactor chat server to include user login and bio management.

--- server/server.js
+++ server/server.js
@@ -1,86 +1,73 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// ---------- SERVE FRONTEND ----------
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static("public"));
 
-// ---------- USERS ----------
-const users = {}; // username -> socket
+const USERS_FILE = "users.json";
 
-// ---------- SOCKET ----------
+let users = {};
+let sockets = {};
+
+if (fs.existsSync(USERS_FILE)) {
+  users = JSON.parse(fs.readFileSync(USERS_FILE));
+}
+
+function saveUsers() {
+  fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
+}
+
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
 
-  // JOIN WITH USERNAME
-  socket.on("join", (username) => {
-    if (!username || users[username]) {
-      socket.emit("joinError", "Username already taken");
+  socket.on("login", ({ username, password }) => {
+
+    if (!users[username]) {
+      users[username] = {
+        password: password,
+        bio: "Hello! I'm new to Texty."
+      };
+      saveUsers();
+    }
+
+    if (users[username].password !== password) {
+      socket.emit("loginError", "Wrong password");
       return;
     }
 
-    socket.username = username;
-    users[username] = socket;
+    sockets[username] = socket.id;
 
-    socket.emit("joinSuccess", username);
-
-    io.emit("chatMessage", {
-      user: "System",
-      message: `${username} joined the chat`
+    socket.emit("loginSuccess", {
+      username: username,
+      bio: users[username].bio
     });
   });
 
-  // GLOBAL CHAT
-  socket.on("chatMessage", (message) => {
-    if (!socket.username) return;
-
-    io.emit("chatMessage", {
-      user: socket.username,
-      message
-    });
+  socket.on("chatMessage", ({ user, message }) => {
+    io.emit("chatMessage", { user, message });
   });
 
-  // DMS
-  socket.on("dm", ({ to, message }) => {
-    if (!socket.username) return;
+  socket.on("dm", ({ from, to, message }) => {
+    const target = sockets[to];
 
-    const target = users[to];
-    if (!target) {
-      socket.emit("chatMessage", {
-        user: "System",
-        message: `User ${to} not found`
-      });
-      return;
+    if (target) {
+      io.to(target).emit("dm", { from, message });
     }
-
-    target.emit("dm", {
-      from: socket.username,
-      message
-    });
   });
 
-  // DISCONNECT
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      delete users[socket.username];
-
-      io.emit("chatMessage", {
-        user: "System",
-        message: `${socket.username} left the chat`
-      });
+  socket.on("updateBio", ({ username, bio }) => {
+    if (users[username]) {
+      users[username].bio = bio;
+      saveUsers();
     }
-
-    console.log("User disconnected:", socket.id);
   });
+
 });
 
-// ---------- PORT (RENDER NEEDS THIS) ----------
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });

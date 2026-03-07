5b5e4d4cce9ed36c4135eb43415213d43ff1579e
Rohan-JD <rohanjdaniel@gmail.com>
2026-03-07 18:11:10 +0530

    Refactor server.js for improved structure and clarity

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const fs = require("fs");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-const fs = require("fs");
-
-let users = {};
-let sockets = {};
 
 app.use(express.static("public"));
 
@@ -19,51 +18,25 @@ const USERS_FILE = "users.json";
 let users = {};
 let sockets = {};
 
+// Load saved users
 if (fs.existsSync(USERS_FILE)) {
   users = JSON.parse(fs.readFileSync(USERS_FILE));
 }
 
+// Save users to file
 function saveUsers() {
   fs.writeFileSync(USERS_FILE, JSON.stringify(users, null, 2));
 }
 
 io.on("connection", (socket) => {
-  socket.on("login", ({ username, password }) => {
 
-  if (!users[username]) {
-    users[username] = {
-      password: password,
-      bio: "Hello! I'm new to TEXTY."
-    };
-  }
-
-  if (users[username].password !== password) {
-    socket.emit("loginError", "Wrong password");
-    return;
-  }
-    socket.on("updateBio", ({ username, bio }) => {
-
-  if (users[username]) {
-    users[username].bio = bio;
-  }
-
-});
-
-  sockets[username] = socket.id;
-
-  socket.emit("loginSuccess", {
-    username: username,
-    bio: users[username].bio
-  });
-
-});
-
+  // LOGIN / REGISTER
   socket.on("login", ({ username, password }) => {
 
     if (!users[username]) {
       users[username] = {
         password: password,
-        bio: "Hello! I'm new to Texty."
+        bio: "Hello! I'm new to TEXTY."
       };
       saveUsers();
     }
@@ -79,12 +52,15 @@ io.on("connection", (socket) => {
       username: username,
       bio: users[username].bio
     });
+
   });
 
+  // GLOBAL CHAT
   socket.on("chatMessage", ({ user, message }) => {
     io.emit("chatMessage", { user, message });
   });
 
+  // DMs
   socket.on("dm", ({ from, to, message }) => {
     const target = sockets[to];
 
@@ -93,18 +69,25 @@ io.on("connection", (socket) => {
     }
   });
 
+  // UPDATE BIO
   socket.on("updateBio", ({ username, bio }) => {
+
     if (users[username]) {
       users[username].bio = bio;
       saveUsers();
     }
+
   });
 
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Render requires using its PORT
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
 
 
 

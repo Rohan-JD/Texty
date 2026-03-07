6bf9550fac8460d08fd2997a0a5bb91eb0a160cd
Rohan-JD <rohanjdaniel@gmail.com>
2026-03-07 18:17:43 +0530

    Remove duplicate imports and clean up server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const fs = require("fs");
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const fs = require("fs");
 
 const app = express();
 const server = http.createServer(app);
@@ -87,6 +83,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
 
 
